22d50e152ac606521989f1f4e9c921d132058ed8
Purichaya Fraczyk <sopeach.a@gmail.com>
2026-04-11 18:19:30 +0700

    refactor: remove unnecessary tags in main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
 resource "aws_vpc" "dev-vpc" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "dev"
+  }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id            = aws_vpc.dev-vpc.id
   cidr_block        = "10.0.10.0/24"
   availability_zone = "ap-southeast-1a"
+  tags = {
+    Name = "subnet-1-dev"
+  }
 }
 
 data "aws_vpc" "existing-vpc" {
@@ -16,4 +22,7 @@ resource "aws_subnet" "dev-subnet-2" {
   vpc_id            = data.aws_vpc.existing-vpc.id
   cidr_block        = "172.31.48.0/24"
   availability_zone = "ap-southeast-1a"
+  tags = {
+    Name = "subnet-defualt"
+  }
 }
