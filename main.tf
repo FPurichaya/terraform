0e43ac0f519cdb4845c8cd3ae27d141e9746feff
Purichaya Fraczyk <sopeach.a@gmail.com>
2026-04-11 17:22:48 +0700

    Refactor: Swap content between main.tf and providers.tf for proper organization

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.40.0"
-    }
-  }
+provider "aws" {
+  region     = "ap-southeast-1"
+}
+
+resource "aws_vpc" "dev-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+  vpc_id            = aws_vpc.dev-vpc.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "ap-southeast-1a"
+}
+
+data "aws_vpc" "existing-vpc" {
+  default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+  vpc_id            = data.aws_vpc.existing-vpc.id
+  cidr_block        = "172.31.48.0/24"
+  availability_zone = "ap-southeast-1a"
 }
