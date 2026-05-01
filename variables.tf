e8c50ddf764aab0d4ad4204d0d3c85487bd7166b
Purichaya Fraczyk <sopeach.a@gmail.com>
2026-05-01 15:38:01 +0700

    Refactor: update variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
 variable "vpc_cidr_block" {}
 variable "private_subnet_cidr_blocks" {}
 variable "public_subnet_cidr_blocks" {}
+variable "avail_zone" {}         
+variable "env_prefix" {}          
+variable "instance_type" {}      
+variable "my_ip" {}             
+variable "public_key_location" {}
